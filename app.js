7a334e678b52bb893920d16173b16bcd55570396
Adria Arroyo Martinez <adria.arroyo.2016@salleurl.edu>
2018-04-05 00:00:03 +0200

    yu

--- app.js
+++ app.js
@@ -3,8 +3,18 @@ var app = express();
 var serv = require('http').Server(app);
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/client/index.html');
+  res.sendFile(__dirname + '/client/ca/index.html');
 });
+app.get('/es', function(req, res) {
+  res.sendFile(__dirname + '/client/es/index.html');
+});
+app.get('/en', function(req, res) {
+  res.sendFile(__dirname + '/client/en/index.html');
+});
+app.get('/zh', function(req, res) {
+  res.sendFile(__dirname + '/client/zh/index.html');
+});
+
 app.use('/client', express.static(__dirname + '/client'));
 
 serv.listen(process.env.PORT || 2000);
