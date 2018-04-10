a2f71d0ec9824e2850b8711d6b36ee1f859730d7
Adria Arroyo Martinez <adria.arroyo.2016@salleurl.edu>
2018-04-10 22:27:59 +0200

    asdf

--- client/main.js
+++ client/main.js
@@ -1,5 +1,56 @@
 
 $(document).ready(function() {
+  $("#content").show();
+
+  $("#desplegableIdiomesContent").hide();
+  $("#desplegableIdiomes").text("Idioma v");
+
+  $("#desplegableIdiomes").click(function(){
+    if(  $("#desplegableIdiomesContent").css("display") == "none"){
+
+        $("#desplegableIdiomesContent").css("display","flex");
+        $("#desplegableIdiomes").text("Idioma ^");
+
+    }else{
+      $("#desplegableIdiomesContent").hide();
+      $("#desplegableIdiomes").text("Idioma v");
+    }
+  });
+
+
+  $("#clickTitle").click(function(){
+    //MAIN STAGE
+  });
+
+
+  $("#nosotros").click(function(){
+
+    $("#nosotrosContent").show();//.css("display","box");
+    $("#calculadoraContent").hide();
+    $("#plantillaContent").hide();
+    $("#blogContent").hide();
+  });
+
+  $("#calculadora").click(function(){
+    $("#nosotrosContent").hide();
+    $("#calculadoraContent").show();//.css("display","box");
+    $("#plantillaContent").hide();
+    $("#blogContent").hide();
+  });
+
+  $("#plantilla").click(function(){
+    $("#nosotrosContent").hide();
+    $("#calculadoraContent").hide();
+    $("#plantillaContent").show();//.css("display","box");
+    $("#blogContent").hide();
+  });
+
+  $("#blog").click(function(){
+    $("#nosotrosContent").hide();
+    $("#calculadoraContent").hide();
+    $("#plantillaContent").hide();
+    $("#blogContent").show();//.css("display","box");
+  });
 
   $("#espanol").click(function(){
     location = "es";
@@ -16,4 +67,7 @@ $(document).ready(function() {
   $("#chinese").click(function(){
     location = "zh";
   });
+
+
+
 });
