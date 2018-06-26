1f6828d99fb3129bd9fb264ab53b4f96f6ebdf4e
Adria Arroyo Martinez <adria.arroyo.2016@salleurl.edu>
2018-06-26 21:38:54 +0200

    animations varias

--- client/main.js
+++ client/main.js
@@ -5,6 +5,9 @@ var lastScrollTop = 0,
   delta = 5;
 var currentScroll = 1;
 
+var currentIdioma = 1;
+var idiomasTriggered = false;
+
 var w = window,
   d = document,
   e = d.documentElement,
@@ -24,7 +27,23 @@ $(document).ready(function() {
     xSize = w.innerWidth || e.clientWidth || g.clientWidth,
     ySize = w.innerHeight || e.clientHeight || g.clientHeight;
 
+    $(".idiomaContainer").children().on('click', function() {
+      currentIdioma = this.id.substring(1);
+      console.log("Current idioma is " + currentIdioma);
+      idiomasTriggered = !idiomasTriggered;
+    });
+    $(".idiomaContainer").children().hover(function() {
+      if(idiomasTriggered === true){
+        console.log("IDIOMAS TRIGGERED AND HOVER");
+      }
+    });
+
   $(".scrollBar").children().on('click', function() {
+    if($('.TopBar').css('opacity') === '0'){
+      $('.TopBar').animate({
+        opacity: 1
+      }, 300, "linear");
+    }
     gestionaScroll(this);
   });
 
@@ -36,7 +55,7 @@ $(document).ready(function() {
       document.documentElement.scrollTop = 0;
       oneTime = false;
     }
-  }, 1000 / 25);
+  }, 1000 / 50);
 
 });
 
@@ -45,29 +64,28 @@ window.onload = function() {
 };
 var aux = 0;
 
-$(window).bind('mousewheel DOMMouseScroll', function(event){
-    if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
-        // scroll up
-        aux += 0.1;
-        if(aux > 1){
-          currentScroll--;
-          if(currentScroll <= 1)
-            currentScroll = 1;
-            gestionaScroll($('#'+currentScroll)[0]);
-          aux = 0;
-        }
+$(window).bind('mousewheel DOMMouseScroll', function(event) {
+  if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
+    // scroll up
+    aux += 0.1;
+    if (aux > 1) {
+      currentScroll--;
+      if (currentScroll <= 1)
+        currentScroll = 1;
+      gestionaScroll($('#' + currentScroll)[0]);
+      aux = 0;
     }
-    else {
-        // scroll down
-        aux += 0.1;
-        if(aux > 1){
-          currentScroll++;
-          if(currentScroll >= 4)
-            currentScroll = 4;
-            gestionaScroll($('#'+currentScroll)[0]);
-          aux = 0;
-        }
+  } else {
+    // scroll down
+    aux += 0.1;
+    if (aux > 1) {
+      currentScroll++;
+      if (currentScroll >= 5)
+        currentScroll = 5;
+      gestionaScroll($('#' + currentScroll)[0]);
+      aux = 0;
     }
+  }
 });
 
 var gestionaScroll = function(elemento) {
@@ -79,7 +97,12 @@ var gestionaScroll = function(elemento) {
       $(".mainSubtitle").css('display', "none");
       $("html, body").animate({
         scrollTop: ySize
-      }, animationSpeed, "swing");
+      }, animationSpeed, "swing", function() {
+        //TOPBAR SHOW!
+        $('.TopBar').animate({
+          opacity: 1
+        }, 300, "linear");
+      });
 
       $(".scrollBar").animate({
         top: 3 * ySize / 2
@@ -90,6 +113,11 @@ var gestionaScroll = function(elemento) {
 
       break;
     case '3':
+      $(".mainTitle").removeClass('animateTopIn')
+
+      $(".mainTitle").css("top","203vh");
+      $(".mainTitle").css("font-size","20px");
+
       $("html, body").animate({
         scrollTop: 2 * ySize
       }, animationSpeed, "swing");
@@ -98,6 +126,8 @@ var gestionaScroll = function(elemento) {
       }, animationSpeed, "swing");
       break;
     case '4':
+      $(".mainTitle").css("top","303vh");
+
       $("html, body").animate({
         scrollTop: 3 * ySize
       }, animationSpeed, "swing");
@@ -105,7 +135,20 @@ var gestionaScroll = function(elemento) {
         top: 7 * ySize / 2
       }, animationSpeed, "swing");
       break;
+    case '5':
+
+      $(".mainTitle").css("top","403vh");
+      $("html, body").animate({
+        scrollTop: 4 * ySize
+      }, animationSpeed, "swing");
+      $(".scrollBar").animate({
+        top: 9 * ySize / 2
+      }, animationSpeed, "swing");
+      break;
     default:
+      $(".mainTitle").css("font-size","50px");
+      $(".mainTitle").css("top","48%");
+
       $(".mainSubtitle").css('display', "block");
       $(".mainTitle").removeClass('animateTopIn');
       $("html, body").animate({
@@ -114,5 +157,9 @@ var gestionaScroll = function(elemento) {
       $(".scrollBar").animate({
         top: ySize / 2
       }, animationSpeed, "swing");
+
+      $('.TopBar').animate({
+        opacity: 0
+      }, 300, "linear");
   }
 }
