750c2c62d3cdfb861ff763114e748f8988d553c7
Adria Arroyo Martinez <adria.arroyo.2016@salleurl.edu>
2018-06-30 18:37:28 +0200

    asdasdasd

--- client/main.js
+++ client/main.js
@@ -1,15 +1,17 @@
+var animationSpeed = 600;
+var titleSize = 80;
+
+
 
 var oneTime = true;
-var animationSpeed = 600;
 
-var lastScrollTop = 0,
-  delta = 5;
-var currentScroll = 1;
 
 var currentIdioma = 1;
 var idiomasTriggered = false;
 var weNeedAnUpadte = false;
+var offsetInit = 0;
 
+var scrollSpeed = 40;
 var w = window,
   d = document,
   e = d.documentElement,
@@ -59,7 +61,9 @@ $(document).ready(function() {
       $(".idiomaContainer").children().css("opacity", "0.5");
     }
     $(this).css("opacity", "1");
-  },function(){$(this).css("opacity", "0.5");});
+  }, function() {
+    $(this).css("opacity", "0.5");
+  });
 
   $(".scrollBar").children().on('click', function() {
     gestionaScroll(this);
@@ -78,106 +82,134 @@ $(document).ready(function() {
       weNeedAnUpadte = false;
     }
   }, 1000 / 50);
-
+  offsetInit = $('#navbar').offset().top;
 });
+
 
 window.onload = function() {
   oneTime = true;
 };
-var aux = 0;
+
+var one = true;
+var dos = true;
 
 $(window).bind('mousewheel DOMMouseScroll', function(event) {
+
   if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
     // scroll up
-    aux += 0.1;
-    if (aux > 1) {
-      currentScroll--;
-      if (currentScroll <= 1)
-        currentScroll = 1;
-      gestionaScroll($('#' + currentScroll)[0]);
-      aux = 0;
+    if(dos){
+      if (window.pageYOffset <= $(".Cara2").offset().top) {
+        gestionaScroll($('#1')[0]);
+        dos = false;
+        one = true;
+      }else{
+        var posicion = window.pageYOffset - scrollSpeed ;
+        $("html, body").scrollTop(posicion);
+      }
+    }else{
+      var posicion = window.pageYOffset - scrollSpeed ;
+      $("html, body").scrollTop(posicion);
     }
+
   } else {
     // scroll down
-    aux += 0.1;
-    if (aux > 1) {
-      currentScroll++;
-      if (currentScroll >= 5)
-        currentScroll = 5;
-      gestionaScroll($('#' + currentScroll)[0]);
-      aux = 0;
+    if (one) {
+      if (window.pageYOffset <= $(".Cara2").offset().top) {
+        gestionaScroll($('#2')[0]);
+        one = false;
+        dos = true;
+      }
+    }else{
+      var posicion = window.pageYOffset + scrollSpeed;
+      $("html, body").scrollTop(posicion);
     }
   }
+
+
+  if (window.pageYOffset >= offsetInit) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+
+  if (window.pageYOffset > $(".Cara2").offset().top) {
+    console.log("caara 2 reached");
+  }
+
+  if (window.pageYOffset > $(".Cara3").offset().top) {
+    console.log("caara 3 reached");
+  }
+  if (window.pageYOffset > $(".Cara4").offset().top) {
+    console.log("caara 4 reached");
+  }
+  if (window.pageYOffset > $(".Cara5").offset().top - 50) {
+    console.log("caara 5 reached");
+  }
+
 });
 
 var gestionaScroll = function(elemento) {
-  //$(".scrollBar").children().find("onZone");
-/*  $(".onZone").addClass("onZoneOut");*/
   $(".scrollBar").children().removeClass('onZone');
   $(elemento).toggleClass('onZone');
 
   switch (elemento.id) {
     case '2':
 
-      $(".mainTitle").css("font-size", "50px");
+      $(".mainTitle").css("font-size", titleSize + "px");
       $(".mainTitle").css("top", "48%");
       $(".mainSubtitle").css('display', "none");
+
       $("html, body").animate({
         scrollTop: ySize
       }, animationSpeed, "swing");
 
-      $(".scrollBar").animate({
-        top: 3 * ySize / 2
-      }, animationSpeed, "swing");
-
-      $(".mainTitle").removeClass('animateTopOut');
-      $(".mainTitle").removeClass('animateTopIn').addClass('animateTopIn');
+      $(".mainTitle").animate({
+        top:ySize + 30,
+        fontSize: '30px'
+      },animationSpeed,"swing",function(){
+        $(".mainTitle").css("display","none");
+        $(".mainTitle2").css("display","block");
+      });
 
       break;
     case '3':
       $(".mainTitle").removeClass('animateTopIn')
 
-      $(".mainTitle").css("top", "203vh");
+      //$(".mainTitle").css("top", "203vh");
       $(".mainTitle").css("font-size", "20px");
 
       $("html, body").animate({
         scrollTop: 2 * ySize
       }, animationSpeed, "swing");
-      $(".scrollBar").animate({
-        top: 5 * ySize / 2
-      }, animationSpeed, "swing");
       break;
     case '4':
-      $(".mainTitle").css("top", "303vh");
+      //$(".mainTitle").css("top", "303vh");
 
       $("html, body").animate({
         scrollTop: 3 * ySize
       }, animationSpeed, "swing");
-      $(".scrollBar").animate({
-        top: 7 * ySize / 2
-      }, animationSpeed, "swing");
       break;
     case '5':
-
-      $(".mainTitle").css("top", "403vh");
+      //$(".mainTitle").css("top", "403vh");
       $("html, body").animate({
         scrollTop: 4 * ySize
       }, animationSpeed, "swing");
-      $(".scrollBar").animate({
-        top: 9 * ySize / 2
-      }, animationSpeed, "swing");
       break;
     default:
-      $(".mainTitle").css("font-size", "50px");
+      $(".mainTitle").css("display","block");
+      $(".mainTitle2").css("display","none");
+
+      $(".mainTitle").css("top","50%");
+      $(".mainTitle").css("font-size","80px");
+
+      $(".mainTitle").css("font-size", titleSize + "px");
       $(".mainTitle").css("top", "48%");
 
       $(".mainSubtitle").css('display', "block");
       $(".mainTitle").removeClass('animateTopIn');
+
       $("html, body").animate({
         scrollTop: 0
-      }, animationSpeed, "swing");
-      $(".scrollBar").animate({
-        top: ySize / 2
       }, animationSpeed, "swing");
   }
 }
@@ -210,8 +242,8 @@ var variaIdioma = function() {
   }
 }
 
-var startImages = function(){
-  $("#img1").css("left","-250px");
-  $("#img2").css("left","300px");
-  $("#img3").css("left","750px");
+var startImages = function() {
+  $("#img1").css("left", "-250px");
+  $("#img2").css("left", "300px");
+  $("#img3").css("left", "750px");
 }
